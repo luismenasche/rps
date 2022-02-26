87f95ee8a5a1dfaeb3e27d6424ae57fbf1494bb8
Luis Menasché <93421667+luismenasche@users.noreply.github.com>
2022-02-26 03:19:09 -0300

    UI Version functional

--- rps.js
+++ rps.js
@@ -1,82 +1,75 @@
-const playArray = ["rock", "paper", "scissors"];
+const playArray = ["\u{1F44A}", "\u270B", "\u270C\uFE0F"];
 
-const pv = document.querySelector(".info__pvalue");
-const cv = document.querySelector(".info__cvalue");
-const res = document.querySelector(".info__result");
-
+const score = document.querySelector(".score");
+const pv = document.querySelector(".score__ppoints");
+const cv = document.querySelector(".score__cpoints");
+const res = document.querySelector(".score__result");
+const selector = document.querySelector(".selector");
 const choices = document.querySelectorAll(".choice");
 for (let c of choices) {
     c.addEventListener("click", playRound);
 }
+const roundNumber = document.querySelector(".round-number");
+const rounds = document.querySelector(".rounds");
+const table = document.querySelector(".rounds__table");
+
+let roundCounter = 1;
 
 function playRound(ev) {
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");
+    td.textContent = `#${roundCounter}`;
+    tr.appendChild(td);
     let playerSelection = Number(ev.currentTarget.getAttribute("data-play"));
+    td = document.createElement("td");
+    td.textContent = playArray[playerSelection];
+    tr.appendChild(td);
     let computerSelection = Math.floor(Math.random() * 3);
-    let newValue, winner;
+    td = document.createElement("td");
+    td.textContent = playArray[computerSelection];
+    tr.appendChild(td);
+    td = document.createElement("td");
     if (playerSelection == computerSelection)
-        return;
-    else if (playerSelection + computerSelection == 2) {
-        if (playerSelection == 0) {
-            winner = 0;
-            newValue = increase(pv);
-        }
-        else {
-            winner = 1;
-            newValue = increase(cv);
-        }
-    }
-    else if (playerSelection - computerSelection > 0) {
-        winner = 0;
-        newValue = increase(pv);
+        td.textContent = "Draw";    
+    else if (((playerSelection == 0) && (computerSelection == 2)) || 
+        (playerSelection == 1) && (computerSelection == 0) ||
+        (playerSelection == 2) && (computerSelection == 1)) {
+            increase(pv);
+            td.textContent = "The Player won this round";
     }
     else {
-        winner = 1;
-        newValue = increase(cv);
+        increase(cv);
+        td.textContent = "The Computer won this round"
     }
-    if (newValue == 5) {
-        for (let c of choices) {
-            c.removeEventListener("click", playRound);
-        }
-        res.style.color = "green";
-        if (winner == 0) {
-            res.textContent = "The Player WON!!!";
-        }
-        else {
-            res.textContent = "The Computer WON!!!";
-        }
-    }
+    tr.appendChild(td);
+    table.appendChild(tr);
+    tr.scrollIntoView();
+    roundCounter++;
+    roundNumber.textContent = String(roundCounter);
+    if (roundCounter == 2)
+        rounds.style.visibility = "visible";
 }
 
 function increase(value) {
     let newValue = Number(value.textContent) + 1;
     value.textContent = String(newValue);
-    return newValue;
-}
-
-/*function game() {
-    let psText, ps, cs, r;
-    let pg = 0;
-    let cg = 0;
-    for (let i = 0; i < 5; i++) {
-        psText = prompt("What is your play?");
-        psText = psText.toLowerCase();
-        ps = playArray.indexOf(psText);
-        cs = computerPlay();
-        console.log(`Game ${i+1}: ${psText} / ${playArray[cs]}`);
-        r = playRound(ps, cs);
-        if (r == 1)
-            pg++;
-        else if (r == -1)
-            cg++;
+    if (newValue == 5) {
+        for (let c of choices) {
+            c.removeEventListener("click", playRound);
+        }
+        res.style.color = "green";
+        if (value.classList.contains("score__ppoints")) {
+            res.innerHTML = "The Player WON!!!<br>(Click here to play again)";
+        }
+        else {
+            res.innerHTML = "The Computer WON!!!<br>(Click here to play again)";
+        }
+        selector.style.display = "none";
+        score.style.width = "100%";
+        score.addEventListener("click", reset, {once: true});
     }
-    console.log(`The player won ${pg} games`);
-    console.log(`The computer won ${cg} games`);
-    if (pg > cg)
-        console.log("The player won!");
-    else if (cg > pg)
-        console.log("The computer won!");
-    else
-        console.log("It ended in a draw!");
 }
 
-game();*/
+function reset() {
+    window.location.reload();
+}
